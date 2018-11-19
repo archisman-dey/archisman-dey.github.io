dc7dec5e7a8293e78341e0a230cd94e45cd1f4c0
archisman-dey <archismandey13@gmail.com>
2018-11-19 21:19:29 +0530

    Added a button to cartoon page

---
+++ script.js
@@ -0,0 +1,32 @@
+dispbutton();
+
+window.addEventListener("resize", function(){dispbutton();});
+
+function dispbutton()
+{
+	var b = document.getElementById('button');
+	var x = window.matchMedia("(max-width: 500px)");
+	if (x.matches)
+	{
+		b.style.display = "none";
+	}
+	else
+	{
+		b.style.display = "inline-block";
+	}
+}
+
+function respond()
+{
+	var b = document.getElementById('button');
+	if (b.innerHTML == "Fit to Page")
+	{
+		document.getElementById("cartoon").style.height = '100vh';
+		b.innerHTML = "Expand";
+	}
+	else if (b.innerHTML == "Expand")
+	{
+		document.getElementById("cartoon").style.height = 'auto';
+		b.innerHTML = "Fit to Page";
+	}	
+}
