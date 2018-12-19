6f5afa8cb88d8da342219ce30bd0c648c39e6fac
archisman-dey <archismandey13@gmail.com>
2018-12-19 18:29:35 +0530

    Added ability to minify

--- update.sh
+++ update.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/zsh
 #Input : commit message
-
+echo "\tMinifying files:\n"
+yui-compressor -v -o script-min.js script.js
+yui-compressor -v -o stylesheet-min.css stylesheet.css
+echo "\n\tCommiting to git:\n"
 git add --all
 git commit -m $1
 echo  #this is just for formatting
