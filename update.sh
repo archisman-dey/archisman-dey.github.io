ded3ef56d51435be4e5137d715c2e34eb60c53c0
archisman-dey <archismandey13@gmail.com>
2019-04-21 20:16:01 +0530

    Redesign

--- update.sh
+++ update.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/zsh
 #Input : commit message
-echo "\tMinifying files:\n"
-yui-compressor -v -o script-min.js script.js
-yui-compressor -v -o stylesheet-min.css stylesheet.css
-echo "\n\tCommiting to git:\n"
+echo "\nCommiting to git:\n"
 git add --all
 git commit -m $1
 echo  #this is just for formatting
